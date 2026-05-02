45142a02ed37b45398642775b60eeb0ca855b505
rickyto21-lab <rickyto21@gmail.com>
2026-05-02 12:53:55 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'toolbox-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果有 cache 就用 cache，冇就正常 fetch
+        return response || fetch(event.request);
+      })
+  );
+});
